79a6c7abc80e5f70a7d08839019d2ea2dc853334
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-05-19 16:42:07 +0600

    Added new toy stored into mongodb

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port= process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
-
 // copy code from mongodb 
 
 console.log(process.env.DB_USER, process.env.DB_PASS);
@@ -29,95 +27,52 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // create db 
+    const toysCollection= client.db("heroHaven").collection("toys");
+    // create db 
+
+
+
+
+
+
+// recieved add toy data from client side 
+app.post('/addToy', async (req, res) => {
+    const newToy = req.body;
+    console.log('adding new toy: ', newToy);
+    const result = await toysCollection.insertOne(newToy);
+    console.log('added count', result.insertedCount);
+    res.send(result)
+});
+// recieved add toy data from client side 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // copy code from mongodb 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // root setup 
 app.get('/', (req, res) => {
