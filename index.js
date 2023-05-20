c6718341ee0013323b647eafad2385e1d7eed2c3
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-05-20 21:12:21 +0600

    deleted from mongo db

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 require("dotenv").config();
-const app = express()
-const port= process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// copy code from mongodb 
+// copy code from mongodb
 
-console.log(process.env.DB_USER, process.env.DB_PASS);
+// console.log(process.env.DB_USER, process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n63ev35.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,37 +20,64 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // create db 
-    const toysCollection= client.db("heroHaven").collection("toys");
-    // create db 
+    // create db
+    const toysCollection = client.db("heroHaven").collection("toys");
+    // create db
+
+    // recieved add toy data from client side
+    app.post("/toys", async (req, res) => {
+      const newToy = req.body;
+      console.log("adding new toy: ", newToy);
+      const result = await toysCollection.insertOne(newToy);
+      console.log("added count", result.insertedCount);
+      res.send(result);
+    });
+
+    // read data from mongo db
+    app.get("/toys", async (req, res) => {
+      const query = {};
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+    // read data from mongo db
+    // find specific data from mongo db
+
+    app.get("/toy", async (req, res) => {
+      const sellerEmail = req.query.sellerEmail;
+      const query = { sellerEmail: sellerEmail };
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // find specific data from mongo db
+
+// delete specific data from mongo db 
+
+app.delete('/toy/:id', async (req, res) => {
+
+const id = req.params.id;
+const query = { _id: new ObjectId(id) };
+const result = await toysCollection.deleteOne(query);
+res.send(result);
 
 
 
 
 
 
-// recieved add toy data from client side 
-app.post('/addToy', async (req, res) => {
-    const newToy = req.body;
-    console.log('adding new toy: ', newToy);
-    const result = await toysCollection.insertOne(newToy);
-    console.log('added count', result.insertedCount);
-    res.send(result)
+
 });
-// recieved add toy data from client side 
 
 
 
-
-
-
+// delete specific data from mongo db 
 
 
 
@@ -63,7 +90,9 @@ app.post('/addToy', async (req, res) => {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -71,15 +100,13 @@ app.post('/addToy', async (req, res) => {
 }
 run().catch(console.dir);
 
+// copy code from mongodb
 
-// copy code from mongodb 
+// root setup
+app.get("/", (req, res) => {
+  res.send("Hero Haven Working");
+});
 
-// root setup 
-app.get('/', (req, res) => {
-    res.send('Hero Haven Working')
-  })
-
-
-  app.listen(port, () => {
-    console.log(`Hero Haven Working on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Hero Haven Working on port ${port}`);
+});
